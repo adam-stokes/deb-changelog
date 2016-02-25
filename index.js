4a61d5840f557b987730f933ea8b1045a7a3fd33
Adam Stokes <adam.stokes@ubuntu.com>
2016-02-25 17:44:13 -0500

    Use class feature, set minimum node == 5
    Signed-off-by: Adam Stokes <adam.stokes@ubuntu.com>

--- index.js
+++ index.js
@@ -1,70 +1,72 @@
 "use strict";
 
-var xre = require("xregexp");
-var debug = require("debug")("deb-changelog");
+const xre = require("xregexp");
+const debug = require("debug")("deb-changelog");
 
-function ChangeLog(blob) {
-    this.blob = blob;
+class ChangeLog {
+    constructor(blob) {
+        this.blob = blob;
+    }
+    parseBody(stanza) {
+        let bodyRe = xre("\\*\\s(?<body>[^\\*-]*)", "img");
+        let matches = [];
+        xre.forEach(stanza, bodyRe, (match, i) => {
+            debug(i);
+            return matches.push(match.body.trim());
+        });
+        return Promise.all(matches);
+    }
+    chunk() {
+        let bodyRe = xre("[-\+]\\d{4}", "mg");
+        let matches = [];
+        let currentIdx = 0;
+        xre.forEach(this.blob, bodyRe, (match, i) => {
+            debug("iter: %s", i);
+            let endIdx = match.index + 6;
+            matches.push(match.input.slice(currentIdx, endIdx).trim());
+            currentIdx = endIdx + 1;
+        });
+        debug(matches);
+        return Promise.all(matches);
+    }
+    parse(stanza) {
+        let entryRe = xre("^(?<pkgname>\\w+)" +
+                          "\\s" +
+                          "\\(" +
+                          "(?<version>[0-9\\-\\.\\+]+)" +
+                          "(?<versionExtra>[\\w\\d\\~\\-\\.\\+]+)" +
+                          "\\)" +
+                          "\\s" +
+                          "(?<series>\\w+);\\surgency=(?<priority>\\w+)" +
+                          "\\s[^]*" +
+                          "--\\s(?<firstname>\\w+)" +
+                          "\\s" +
+                          "(?<lastname>\\w+)" +
+                          "\\s" +
+                          "(?<email><.*>)" +
+                          "\\s+" +
+                          "(?<timestamp>.*)", "img");
+        let match = xre.exec(stanza, entryRe);
+        let model = {
+            pkgname: match.pkgname,
+            version: match.version,
+            versionExtra: match.versionExtra,
+            series: match.series,
+            priority: match.priority,
+            firstname: match.firstname,
+            lastname: match.lastname,
+            email: match.email,
+            timestamp: match.timestamp
+        };
+        this.parseBody(stanza)
+            .then((res) => {
+                model.body = res;
+            });
+        return Promise.resolve(model);
+    }
 }
 
-ChangeLog.prototype.parseBody = function(stanza) {
-    var bodyRe = xre("\\*\\s(?<body>[^\\*-]*)", "img");
-    var matches = [];
-    xre.forEach(stanza, bodyRe, function(match, i) {
-        debug(i);
-        return matches.push(match.body.trim());
-    });
-    return Promise.all(matches);
-};
-
-ChangeLog.prototype.chunk = function() {
-    var bodyRe = xre("[-\+]\\d{4}", "mg");
-    var matches = [];
-    var currentIdx = 0;
-    xre.forEach(this.blob, bodyRe, function(match, i) {
-        debug("iter: %s", i);
-        var endIdx = match.index + 6;
-        matches.push(match.input.slice(currentIdx, endIdx).trim());
-        currentIdx = endIdx + 1;
-    });
-    debug(matches);
-    return Promise.all(matches);
-};
-
-ChangeLog.prototype.parse = function(stanza) {
-    var entryRe = xre("^(?<pkgname>\\w+)" +
-                      "\\s" +
-                      "\\(" +
-                      "(?<version>[0-9\\-\\.\\+]+)" +
-                      "(?<versionExtra>[\\w\\d\\~\\-\\.\\+]+)" +
-                      "\\)" +
-                      "\\s" +
-                      "(?<series>\\w+);\\surgency=(?<priority>\\w+)" +
-                      "\\s[^]*" +
-                      "--\\s(?<firstname>\\w+)" +
-                      "\\s" +
-                      "(?<lastname>\\w+)" +
-                      "\\s" +
-                      "(?<email><.*>)" +
-                      "\\s+" +
-                      "(?<timestamp>.*)", "img");
-    var match = xre.exec(stanza, entryRe);
-    var model = {
-        pkgname: match.pkgname,
-        version: match.version,
-        versionExtra: match.versionExtra,
-        series: match.series,
-        priority: match.priority,
-        firstname: match.firstname,
-        lastname: match.lastname,
-        email: match.email,
-        timestamp: match.timestamp
-    };
-    this.parseBody(stanza)
-        .then(function(res){
-            model.body = res;
-        });
-    return Promise.resolve(model);
-};
-
 module.exports = ChangeLog;
+
+
+
--- test.js
+++ test.js
@@ -1,84 +1,84 @@
 /* global describe it */
 "use strict";
-var DC = require(".");
-var co = require("co");
-var _ = require("lodash");
+const DC = require(".");
+const co = require("co");
+const _ = require("lodash");
 require("should");
 
-var testData = "macumba (0.6-0ubuntu1) trusty; urgency=medium\n\n" +
+const testData = "macumba (0.6-0ubuntu1) trusty; urgency=medium\n\n" +
     "* Fix threaded execution\n" +
     "* More fixes\n" +
     "  Spans additional line\n" +
     "* Tartar sauce\n\n" +
     " -- Adam Stokes <adam.stokes@ubuntu.com>  Thu, 14 May 2015 08:43:11 -0400";
 
-describe("deb-changelog", function(){
-    var cl = new DC(testData);
+describe("deb-changelog", () => {
+    let cl = new DC(testData);
     function* parse(){
-        var chunks = yield cl.chunk();
+        let chunks = yield cl.chunk();
         return yield cl.parse(_.first(chunks));
     }
 
-    it("should parse version", function(done){
+    it("should parse version", (done) => {
         co(function*(){
-            var res = yield parse();
+            let res = yield parse();
             res.version.should.equal("0.6-0");
             done();
-        }).catch(function(err){
+        }).catch((err) => {
             done(err);
         });
     });
-    it("should parse package name", function(done){
+    it("should parse package name", (done) => {
         co(function*(){
-            var res = yield parse();
+            let res = yield parse();
             res.pkgname.should.equal("macumba");
             done();
-        }).catch(function(err){
+        }).catch((err) => {
             done(err);
         });
     });
-    it("should parse version extra", function(done){
+    it("should parse version extra", (done) => {
         co(function*(){
-            var res = yield parse();
+            let res = yield parse();
             res.versionExtra.should.equal("ubuntu1");
             done();
-        }).catch(function(err){
+        }).catch((err) => {
             done(err);
         });
     });
-    it("should parse series name", function(done){
+    it("should parse series name", (done) => {
         co(function*(){
-            var res = yield parse();
+            let res = yield parse();
             res.series.should.equal("trusty");
             done();
-        }).catch(function(err){
+        }).catch((err) => {
             done(err);
         });
     });
-    it("should parse priority name", function(done){
+    it("should parse priority name", (done) => {
         co(function*(){
-            var res = yield parse();
+            let res = yield parse();
             res.priority.should.equal("medium");
             done();
-        }).catch(function(err){
+        }).catch((err) => {
             done(err);
         });
     });
-    it("should parse timestamp", function(done){
+    it("should parse timestamp", (done) => {
         co(function*(){
-            var res = yield parse();
+            let res = yield parse();
             res.timestamp.should.equal("Thu, 14 May 2015 08:43:11 -0400");
             done();
-        }).catch(function(err){
+        }).catch((err) => {
             done(err);
         });
     });
-    it("should contain a body array", function(done){
+    it("should contain a body array", (done) => {
         co(function*(){
-            var res = yield parse();
+            let res = yield parse();
             res.body.length.should.equal(3);
             done();
-        }).catch(function(err){
+        }).catch((err) => {
             done(err);
         });
     });
